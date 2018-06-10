e0c340ceed292d6fd518a7ba471c59a2de1e6996
xiaobao <515122644@qq.com>
2018-06-11 00:12:38 +0800

    initialized spring boot demo

---
+++ src/main/java/com/nick/springbootdemo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.nick.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String index(String name) {
+        return "Hello " + name;
+    }
+}
