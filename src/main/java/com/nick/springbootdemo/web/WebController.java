006353d21d06f8de94c658ebf90ff52b107632a4
Nick <hejl@cn.ibm.com>
2018-06-26 14:17:33 +0800

    RestTemplate post 方式提交 raw（任意格式的数据） 数据。

--- src/main/java/com/nick/springbootdemo/web/WebController.java
+++ src/main/java/com/nick/springbootdemo/web/WebController.java
@@ -1,12 +1,20 @@
 package com.nick.springbootdemo.web;
 
 import com.nick.springbootdemo.domain.User;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -47,5 +55,19 @@ public class WebController {
                 System.out.println(error.getCode() + "-" + error.getDefaultMessage());
             }
         }
+    }
+
+    @RequestMapping("/booking")
+    public String requestBooking() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        String params = "{\n" +
+                "  \"api_key\": \"4A2D1BE1-DF26-4B29-990C-85C751BE1260\",\n" +
+                "  \"secret_key\": \"90d9d4d2e1844aaad1114da62f42b9c46ac9720b831c517f641175d81cc8287b\"\n" +
+                "}";
+        HttpEntity<String> requestEntity = new HttpEntity(params, httpHeaders);
+        String res = restTemplate.postForObject("http://13.230.241.58:8189/rest/gateio/getBalance", requestEntity, String.class);
+        return res;
     }
 }
